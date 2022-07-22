b152dae8a48022008791322200b3fe527dce1638
Matt Davis <mjdavis74@gmail.com>
2022-07-22 16:29:01 -0400

    Completed module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,16 +1,1 @@
 import Expense
-import collections
-
-
-expenses = Expense.Expenses()
-    expenses.read_expenses(data/spending_data.csv)
-
-def spending_categories():
-    for expense in expenses.list:
-        spending_categories.append(expense.category)
-
-
-def spending_counter():
-    collections.Counter(spending_categories)
-        print(spending_counter)
-    spending_counter.most_common(5)

5cc8a36c964aab09fa48d02233df560a55012394
Matt Davis <mjdavis74@gmail.com>
2022-07-23 11:59:12 -0400

    Complete module

--- budget/FrequentExpenses.py
+++ budget/FrequentExpenses.py
@@ -1,1 +1,12 @@
-import Expense
+from . import Expense
+
+
+def expenses():
+    expenses = Expense.Expenses()
+    expenses.read_expenses(data/spending_data.csv)
+
+
+def spending_categories([]):
+    for expense in expenses.list:
+        spending_categories.append(expense.category)
+        
